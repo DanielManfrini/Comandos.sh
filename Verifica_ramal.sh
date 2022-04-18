0f9fb8d9fbb1f1b2bbfb047b24cb25249add22d9
Daniel Lopes Manfrini <103941190+Ygfils@users.noreply.github.com>
2022-04-18 11:24:14 -0300

    Update Verifica_ramal.sh

--- Verifica_ramal.sh
+++ Verifica_ramal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-	# O script abaixo avisa a quantidade de desconexões de um ramal no dia
-  # pode ser alterado para IP; HOSTNAME & Usuário dependendo do seu log gerado.
+  # O script abaixo avisa a quantidade de desconexões de um ramal no dia
+  # pode ser alterado para IP; HOSTNAME ou Usuário dependendo do seu log gerado.
   # atualmente trabalhado em um server de telefonia asterix 
   # altere os nomes de variaveis e caminhos conforme a nescessidade.
 
@@ -16,3 +16,9 @@
 
    QUANTIDADE=$(cat /script/backup_logs/messages/messages_$DIA |grep $Ramal | grep  "UNRE" -c)
    echo "o ramal teve $QUANTIDADE desconexões hoje"
+
+# pode ser integrado também ao zabbix para monitoração e alerta.
+
+UserParameter=conta.quedas[*],DIA=$(date +'%d-%m-%Y'); cat /script/backup_logs/messages/messages_$DIA |grep "$1" | grep "UNRE" -c
+
+# Ele irá retornar a quantidade de quedas que o Ramal; IP; HOSTNAME ou Usuário teve no dia atrávés do log gerado. 
